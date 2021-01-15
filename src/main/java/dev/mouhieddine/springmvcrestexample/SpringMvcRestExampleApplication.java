5f0c739ab3056a6475b5349407e4f7e40156624b
BIRSAx2 <Mouhieddinesabir@gmail.com>
2021-01-15 15:42:47 +0100

    Initial commit

---
+++ src/main/java/dev/mouhieddine/springmvcrestexample/SpringMvcRestExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.mouhieddine.springmvcrestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMvcRestExampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/mouhieddine/springmvcrestexample/SpringMvcRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mouhieddine.springmvcrestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
